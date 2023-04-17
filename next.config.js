cf59cbb0f1107450c10c8b8fb99d39f96d2a5869
Clifftek <jakob.johnson.dev@gmail.com>
2023-04-17 20:59:45 +1000

    initial commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching
+})
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  reactStrictMode: true
+});
+
+module.exports = nextConfig
